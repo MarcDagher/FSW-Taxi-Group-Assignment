1f84a5a91b5546602fd155967131c5e85cfcb7f1
MarcDagher <marcdagher47@gmail.com>
2023-12-11 22:40:13 +0200

    [core] deletePassenger

--- taxi-backend/app/Http/Controllers/PassengersController.php
+++ taxi-backend/app/Http/Controllers/PassengersController.php
@@ -5,29 +5,43 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redis;
 
 class PassengersController extends Controller
 {
     // role id = 3 - create/signup for a passenger account 
     // request => "user_id","email","password","first_name","last_name","role_id"
-    public function createPassenger(Request $request){
-        $user = User::where('email', $request -> email)->first();
 
-        if ($user){
-            return response()->json([
-                'status' => 'failed',
-                'message' => 'User email already exists'
-            ]);
-        } else {
-                DB::table('users')->insert([
-                    'email' => $request -> email,
-                    'password' => bcrypt($request -> password),
-                    'first_name' => $request -> first_name,
-                    'last_name' => $request -> last_name,
-                    'role_id' => 3
+    public function createPassenger(Request $request){
+
+        $request->validate(['email' => 'required|string|email|unique:users', 'password' => 'required|string']);
+            DB::table('users')->insert([
+                'email' => $request -> email,
+                'password' => bcrypt($request -> password),
+                'first_name' => $request -> first_name,
+                'last_name' => $request -> last_name,
+                'role_id' => 3]);
+                return response() -> json([
+                    'status' => 'success',
+                    'message' => 'User added successfuly',
                 ]);
-                echo "Passenger added successfully";
-            // 'email' => $request -> email,
-        } 
+        }
+
+    public function deletePassenger(Request $request){
+        $user = User::where('email', $request -> email) -> first();
+
+        if($user){
+            $user -> delete();            
+            return response() -> json([
+                'status' => 'success',
+                'message' => 'User deleted successfuly',
+            ]);
+
+        } else {
+            return response() -> json([
+                'status' => 'failed',
+                'message' => 'User not found',
+            ]);
+        }
     }
 }
