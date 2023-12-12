3dcb00aaed0f65f563521cee39479a3808304f31
rawadabdallah <rawad0ne@gmail.com>
2023-12-12 16:13:42 +0200

    [core] Adding pages and mockdata for Admin

--- taxi-frontend/src/App.jsx
+++ taxi-frontend/src/App.jsx
@@ -2,15 +2,20 @@
 import { useState } from "react";
 import Button from "./components/ui/button";
 import Header from "./components/ui/header";
-import "./App.css";
+import Home from "./pages/Home";
+import Admin from "./pages/Admin";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
-    return (
-        <>
-
-            <Header />
-        </>
-    );
+  return (
+    <BrowserRouter>
+      <Routes> 
+        <Route path="/" index element={<Home />} />
+        <Route path="/admin" index element={<Admin />} />
+        <Route path="*" element={<>404 page not found</>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
