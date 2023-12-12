0ae45c730965a99e6491859db859b439f10c1d7c
rawadabdallah <rawad0ne@gmail.com>
2023-12-12 21:40:20 +0200

    [core] Ratings api

---
+++ taxi-backend/app/Http/Controllers/RatingsController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Rating;
+use App\Models\Ride;
+
+class RatingsController extends Controller
+{
+
+    public function addRating(Request $request)
+    {
+        if (Auth::check()) {
+            $user = Auth::user();
+            //check if rating.user_id equal to $user->user_id
+            $rating = Rating::where('user_id', $user->user_id)
+            ->where('rating_for', $request->rating_for)
+            ->get();
+
+            if($rating->count() > 0){
+                return response()->json(['message' => 'You have already rated this ride'], 422);
+            }
+
+            $ride = Ride::where('user_id', $user->user_id)
+                ->where('ride_id', $request->ride_id)
+                ->get();
+            
+
+            if ($ride->count() > 0) {
+                $rating = new Rating();
+                $rating->rating = $request->rating;
+                $rating->rating_by = $request->user_id;
+                $rating->rating_for = $request->rating_for;
+                $rating->ride_id = $request->ride_id;
+                $rating->save();
+
+                return response()->json([
+                    'status' => 'status',
+                    'message' => 'Rating added successfully',
+                ]);
+            } else {
+                return response()->json([
+                    'status' => 'status',
+                    'message' => 'Ride not found',
+                ]);
+            }
+
+        } else {
+            return response()->json([
+                'status' => 'failed',
+                'error' => 'Unauthorised'
+            ], 401);
+        }
+    }
+
+}
--- taxi-backend/routes/api.php
+++ taxi-backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RatingsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -25,4 +26,8 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
 
+});
+
+Route::controller(RatingsController::class)->group(function(){
+    Route::post('addRating', 'addRating');
 });
