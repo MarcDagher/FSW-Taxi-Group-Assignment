577a311be621b585ab2b027b1b550ead76b3cceb
rawadabdallah <rawad0ne@gmail.com>
2023-12-12 13:30:43 +0200

    [bug-fix] Fixing null user

--- taxi-backend/app/Models/User.php
+++ taxi-backend/app/Models/User.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
@@ -11,60 +12,34 @@ class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $key = 'user_id';
-
+    protected $primaryKey = "user_id";
     protected $fillable = [
-        'name',
-        'email',
-        'password',
         'first_name',
         'last_name',
-        'role_id',
-        'created_at',
-        'updated_at',
+        'email',
+        'password',
+        'img',
+        'role_id'
     ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
 
-    /**
-     * Get the identifier that will be stored in the subject claim of the JWT.
-     *
-     * @return mixed
-     */
     public function getJWTIdentifier()
     {
-        return $this->key;
+        return $this->user_id;
     }
-    /**
-     * Return a key value array, containing any custom claims to be added to the JWT.
-     *
-     * @return array
-     */
+
     public function getJWTCustomClaims()
     {
-        return [];
+        return [
+            'user_id' => $this->user_id,
+        ];
     }
 
 }
