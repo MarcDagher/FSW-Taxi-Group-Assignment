922c8ef62cf5921902ceacf2832c2f1a429522fc
rawadabdallah <rawad0ne@gmail.com>
2023-12-12 17:45:19 +0200

    [core]

--- taxi-frontend/src/App.jsx
+++ taxi-frontend/src/App.jsx
@@ -1,22 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import HomePage from './Pages/Homepage/homepage';
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import Button from "./components/ui/button";
+import Header from "./components/ui/header";
+import Home from "./pages/Home";
+import Admin from "./pages/Admin";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <BrowserRouter>
-          <Routes>
-          <Route path="/" element={<HomePage />} />
-          </Routes>
-        </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes> 
+        <Route path="/" index element={<Home />} />
+        <Route path="/admin" index element={<Admin />} />
+        <Route path="*" element={<>404 page not found</>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
