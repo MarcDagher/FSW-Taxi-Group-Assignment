02733595ace88f1f489f5423c773fca79c37d9ad
Chahine-Chahine <chahinesquare@hotmail.com>
2023-12-13 02:35:16 +0200

    [core]: fetched google map api and placed styling

---
+++ taxi-frontend/src/Pages/MapPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GoogleApiWrapper from '../Components/Maps';
+
+function MapPage() {
+  return (
+    <div className="container">
+        <GoogleApiWrapper/>
+      </div>
+  );
+}
+
+export default MapPage;
