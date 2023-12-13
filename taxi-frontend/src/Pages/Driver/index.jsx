a3a6f861ad6a31e2729fd33796540cbb4c7cd9fd
Chahine-Chahine <chahinesquare@hotmail.com>
2023-12-14 01:59:52 +0200

    [bug-fix]:merge error correction

--- taxi-frontend/src/Pages/Driver/index.jsx
+++ taxi-frontend/src/Pages/Driver/index.jsx
@@ -1,13 +1,21 @@
-import HeaderHome from "../../components/ui/header";
-import LogoTextComponent from "../../components/ui/popup";
+import Header from "../../components/ui/header";
+import WelcomeDriver from "../../components/Driver/Welcome";
+import RideCard from "../../components/Common/card";
 
-function Drivereview(){
-    return (
- 
-        <div>
-        <HeaderHome/>
-        <LogoTextComponent text1={"Currently Your profile is under review by admin"} text2={"We’ll contact you soon" }/> 
-        </div>
-    )
+
+function RidesList(){
+
+
+    return(
+        <>
+        <Header/>
+        <WelcomeDriver welcome={"Chahine"}/>
+        <RideCard request={12} name = {"Rony"} location = {"Beirut hamra"} destination = {"BDD"} price = {"8$"}/>
+        <RideCard request={13} name = {"Chahine"} location = {"Doha"} destination = {"BDD"} price = {"15$"}/>
+        <RideCard request={14} name = {"Rawad"} location = {"Baalback"} destination = {"BDD"} price = {"60$"}/>
+        <RideCard request={15} name = {"Marc"} location = {"ajaalton"} destination = {"BDD"} price = {"60$"}/>
+        </> 
+    );
 }
-export default Drivereview;
+
+export default RidesList;
