cf13631939fd862379c91eff23a312908e872212
MarcDagher <marcdagher47@gmail.com>
2023-12-13 04:10:15 +0200

    [core] Refine header

--- taxi-frontend/src/helpers/request_helper.js
+++ taxi-frontend/src/helpers/request_helper.js
@@ -3,6 +3,9 @@ import axios from 'axios'
 axios.defaults.baseURL = "http//127.0.0.1/api"
 
 export const request = async ({route, method = "GET", body}) =>{
-    const response = await axios.request({url: route, method: method, data: body,})
+    const response = await axios.request({
+      url: route, 
+      method: method, 
+      data: body,})
     return response
 }
