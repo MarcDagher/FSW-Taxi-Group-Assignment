2975d90fb3aa93c65f39003c111a87cd5f4af29e
Chahine-Chahine <chahinesquare@hotmail.com>
2023-12-14 04:15:49 +0200

    [style]: implemented styles for homepage

--- taxi-frontend/src/pages/Passenger Dashboard/index.jsx
+++ taxi-frontend/src/pages/Passenger Dashboard/index.jsx
@@ -1,14 +1,48 @@
 import "./index.css"
 import Header from "../../components/ui/header"
 import Button from "../../components/ui/button"
-
+import request from "../../helpers/request_helper"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 const PassengerDashboard = () => {
+
+const token = localStorage.getItem('token')  
+
+const [requestError, setRequestError] = useState(false)
+const navigate = useNavigate()
+if (!token){
+  navigate("/error")
+}
+const [formData, setFormData] = useState({ 
+  pickup_location : "", 
+  destination_location : "",
+})
+
+const handleChange = (name, value) => {
+    setFormData((previous)=> {return {...previous, [name] : value}})
+}
+
+
+const handleSubmit = async () => {
+
+  const response = await request({
+      body: {...formData},
+      route: "request_ride",
+      method: "POST",
+      token: token})
+
+  if (response.data.status == "success"){
+    setRequestError(false)
+    navigate("/PassengerRequest")
+  } else if (response.data.status == "failed"){
+     setRequestError(true)
+  }
+}
 
   return <>
   <div className="wrapper">
     <Header />
-  
       <div className="box">
         <div className="inputs-col">
           <div className="text-col">
@@ -16,11 +50,12 @@ const PassengerDashboard = () => {
             <p>Book a Ride</p>
           </div>
           <div className="input-container">
-              <input type="text" name="pickup" placeholder="Pickup"/>
-              <input type="text" name="destintion" placeholder="Destination"/>
+              {requestError && <p className="request_error">Ride already exists!</p>}
+              <input type="text" name="pickup_location" placeholder="Pickup" onChange={(e) => handleChange("pickup_location", e.target.value)}/>
+              <input type="text" name="destintion" placeholder="Destination" onChange={(e) => handleChange("destination_location", e.target.value)}/>
           </div>
         </div>
-          <Button>Request</Button>
+          <div onClick={handleSubmit}><Button>Request</Button></div>
     </div>
   </div>
   </>
